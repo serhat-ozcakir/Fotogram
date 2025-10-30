2e69b97c1c8ea61063dc51bfa137a7c1c2b22a96
serhat özcakir <serhatozcakir35@gmail.com>
2025-10-30 09:08:16 +0100

    update comit

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Image data array
+const imagesData = [
+    { "id": 1, "name": "Der freche Flügelfreund", "url": "./img/bird-7233900_1280.jpg" },
+    { "id": 2, "name": "Sakura im Frühlingstraum", "url": "./img/cherry-blossoms-7766587_1280.jpg" },
+    { "id": 3, "name": "Horizont der Sehnsucht", "url": "./img/landscape-8722691_1280.jpg" },
+    { "id": 4, "name": "Fluss der Ruhe", "url": "./img/river-7212351_1280.jpg" },
+    { "id": 5, "name": "Grünes Geheimnis", "url": "./img/forest-3369950_1280.jpg" },
+    { "id": 6, "name": "Wald des Morgenlichts", "url": "./img/forest-7406241_1280.jpg" },
+    { "id": 7, "name": "Das Salzgebirge", "url": "./img/salt-moutain-2767408_1280.jpg" },
+    { "id": 8, "name": "Wüste aus Feuer", "url": "./img/arizona-5032415_1280.jpg" },
+    { "id": 9, "name": "Pfad ins Unbekannte", "url": "./img/pathway-2289978_1280.jpg" },
+    { "id": 10, "name": "Sonnenglanz im Wald", "url": "./img/sunlight-3130638_1280.jpg" },
+    { "id": 11, "name": "Abendrotpoesie", "url": "./img/sunset-4086848_1280.jpg" },
+    { "id": 12, "name": "Wintergasse", "url": "./img/montestrutto-190471_1280.jpg" },
+    { "id": 13, "name": "Träume über den Wolken", "url": "./img/airplane-1807486_1280.jpg" },
+    { "id": 14, "name": "Wiesenglück", "url": "./img/grass-6545509_1280.jpg" },
+    { "id": 15, "name": "Weiße Weite", "url": "./img/salt-3344508_1280.jpg" },
+    { "id": 16, "name": "Die schläfrige Schafparade", "url": "./img/sheep-9401445_1280.webp" },
+    { "id": 17, "name": "Winterzauberland", "url": "./img/winter-8612635_1280.jpg" },
+    { "id": 18, "name": "Himmelsspiegel", "url": "./img/qinghai-2825151_1280.jpg" }
+]
+
 // Get references to DOM elements
 const dialogRef = document.getElementById("myDialog");
 const dialogImg = document.getElementById("dialogImg");
@@ -6,8 +28,29 @@ const imgTitle = document.getElementById("imgtitle");
 const next = document.getElementById("next");
 const back = document.getElementById("back");
 
-let imagesData = [];
+// Current index of the displayed image
 let currentIndex = 0;
+
+// Function to generate image grid
+function myFunction() {
+    const imageContainer = document.getElementById("image-div");
+    imagesData.forEach((element, i) => {
+        imageContainer.innerHTML += `
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 box">
+      <img src="${element.url}" alt="${element.name}" class="image" data-index="${i}">
+    </div>
+  `;
+    });
+    const images = document.querySelectorAll("#image-div img");
+    images.forEach(img => {
+        img.addEventListener("click", (event) => {
+            currentIndex = Number(event.target.getAttribute("data-index"));
+            updateDialog();
+            openDialog();
+        });
+    });
+}
+myFunction();
 
 // Open dialog functions
 function openDialog() {
@@ -18,9 +61,8 @@ function openDialog() {
 
 // Close dialog with animation
 function closeDialog() {
-   dialogRef.classList.remove("opened");
-    setTimeout(() => dialogRef.close(), 300); 
-       document.addEventListener("keydown", handleKeyDown);
+    dialogRef.classList.remove("opened");
+    dialogRef.close();
 }
 
 // Update dialog content based on current index
@@ -32,39 +74,8 @@ function updateDialog() {
     Counter.textContent = `${currentIndex + 1} / ${imagesData.length}`;
 }
 
-// Fetch image data from JSON file and populate the gallery
-function myFunction() {
-    fetch("./image.json")
-        .then(response => response.json())
-        .then(response => {
-            imagesData = response;
-            const imageContainer = document.getElementById("image-div");
-            imageContainer.innerHTML = "";
-
-            for (let i = 0; i < response.length; i++) {
-                const element = response[i];
-                console.log(element);
-                imageContainer.innerHTML += `
-                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 box">
-                    <img src="${element.url}" alt="${element.name}"  class="image" data-index="${i}">
-                </div>
-                `;
-            }
-            const images = document.querySelectorAll("#image-div img");
-            images.forEach(img => {
-                img.addEventListener("click", (event) => {
-                    currentIndex = Number(event.target.getAttribute("data-index"));
-                    updateDialog();
-                    openDialog();
-                });
-            });
-        })
-        .catch(error => console.error("Veri okunurken hata:", error));
-}
-
-// Next button just change index once per click
-next.addEventListener("click", (event) => {
-    event.preventDefault();
+// Next button functionality
+next.addEventListener("click", () => {
     currentIndex++;
     if (currentIndex >= imagesData.length) {
         currentIndex = 0;
@@ -73,15 +84,13 @@ next.addEventListener("click", (event) => {
 });
 
 // Back button functionality
-back.addEventListener("click", (event) => {
-    event.preventDefault();
+back.addEventListener("click", () => {
     currentIndex--;
     if (currentIndex < 0) {
         currentIndex = imagesData.length - 1;
     }
     updateDialog();
 });
-myFunction();
 
 // Close dialog when clicking outside the image
 dialogRef.addEventListener("click", (event) => {
@@ -94,7 +103,7 @@ dialogRef.addEventListener("click", (event) => {
 function handleKeyDown(event) {
     if (event.key === "ArrowRight") {
         currentIndex++;
-        if(currentIndex >= imagesData.length) {
+        if (currentIndex >= imagesData.length) {
             currentIndex = 0;
         }
         updateDialog();
